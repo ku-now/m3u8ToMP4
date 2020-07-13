ffe1551ded9c9aa23a80a386dcf7f0bbb3522bd1
ku-now <kuwa52004@gmail.com>
2020-07-14 04:12:38 +0900

    v1.1.1

--- m3u8ToMP4/Program.cs
+++ m3u8ToMP4/Program.cs
@@ -32,7 +32,7 @@ namespace m3u8ToMP4
                 clip = data.GetData(DataFormats.Text) as string;
             }
 
-            if ((Path.GetExtension(clip) != ".m3u8") ||
+            if ((clip.IndexOf(".m3u8", StringComparison.OrdinalIgnoreCase) < 0) ||
                 (clip.IndexOf("http", StringComparison.OrdinalIgnoreCase) < 0))
             {
                 MessageBox.Show("クリップボードにコピーされたURLがm3u8ファイルのものでないか、URLではありません。\n" +
@@ -41,7 +41,7 @@ namespace m3u8ToMP4
                                 "クリップボードの内容: " + clip, "m3u8 -> mp4 | mp3 | m4a");
                 return;
             }
-            
+
             SaveFileDialog sfd = new SaveFileDialog();
 
             sfd.Filter = "MP4ファイル(*.mp4)|*.mp4|MP3ファイル(*.mp3)|*.mp3|M4Aファイル(*.m4a)|*.m4a|すべてのファイル(*.*)|*.*";
@@ -55,22 +55,22 @@ namespace m3u8ToMP4
                 ProcessStartInfo app = new ProcessStartInfo();
 
                 app.FileName = "ffmpeg.exe";
+                app.Arguments = "-protocol_whitelist file,http,https,tcp,tls,crypto -i \"" + clip + "\" ";
 
                 if (text == ".mp3")
                 {
-                    app.Arguments = "-i \"" + clip + "\" -write_xing 0 \"" + sfd.FileName + "\"";
+                    app.Arguments += "-write_xing 0";
                 }
                 else if (text == ".m4a")
                 {
-                    app.Arguments = "-i \"" + clip + "\" -c copy \"" + sfd.FileName + "\"";
+                    app.Arguments += "-c copy";
                 }
                 else
                 {
-                    app.Arguments = "-protocol_whitelist file,http,https,tcp,tls,crypto -i \"" + clip + "\" " +
-                                    "-headers \'User-Agent:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36\"\' " +
-                                    "-movflags faststart -c copy -bsf:a aac_adtstoasc \"" + sfd.FileName + "\"";
+                    app.Arguments += "-movflags faststart -c copy -bsf:a aac_adtstoasc";
                 }
 
+                app.Arguments += " \"" + sfd.FileName + "\"";
                 app.UseShellExecute = true;
 
                 Process.Start(app);
